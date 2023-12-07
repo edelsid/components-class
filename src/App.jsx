92b952d629b4e2affc8b65de9e216cf3a962b914
Xenia Ch <eradonna@yandex.ru>
2023-12-07 05:32:05 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './css/main.css';
+import { ProductCC } from './components/ProductCC';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ProductCC item={item} />  
+      </div>
+    </div>
+  )
+}
+
+export default App
